0c5232c63154a9647cea7a7c63d84cd3ced37679
correy <112426112@qq.com>
2024-02-27 21:44:42 +0800

    改进。

--- src/scan/icmp.cpp
+++ src/scan/icmp.cpp
@@ -1,1 +1,50 @@
 #include "icmp.h"
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+int Icmpv4Scan(PIN_ADDR SrcIPv4, PIN_ADDR DstIPv4)
+{
+    int ret = 0;
+
+    UNREFERENCED_PARAMETER(SrcIPv4);
+    UNREFERENCED_PARAMETER(DstIPv4);
+
+    return ret;
+}
+
+
+int Icmpv4Scan(PIN_ADDR SrcIPv4, PIN_ADDR DstIPv4, BYTE Mask)
+{
+    int ret = 0;
+
+    UNREFERENCED_PARAMETER(SrcIPv4);
+    UNREFERENCED_PARAMETER(DstIPv4);
+    UNREFERENCED_PARAMETER(Mask);
+
+    return ret;
+}
+
+
+int Icmpv6Scan(PIN6_ADDR SrcIPv6, PIN6_ADDR DstIPv6)
+{
+    int ret = 0;
+
+    UNREFERENCED_PARAMETER(SrcIPv6);
+    UNREFERENCED_PARAMETER(DstIPv6);
+
+    return ret;
+}
+
+
+int Icmpv6Scan(PIN6_ADDR SrcIPv6, PIN6_ADDR DstIPv6, BYTE Mask)
+{
+    int ret = 0;
+
+    UNREFERENCED_PARAMETER(SrcIPv6);
+    UNREFERENCED_PARAMETER(DstIPv6);
+    UNREFERENCED_PARAMETER(Mask);
+
+    return ret;
+}
--- src/scan/icmp.h
+++ src/scan/icmp.h
@@ -1,5 +1,14 @@
-#pragma once
-class icmp
-{
-};
+/*
+功能：ICMP扫描。
 
+最基本和强大的ICMP扫描是自己组装ICMP，其次是用API。
+*/
+
+#pragma once
+
+#include "pch.h"
+
+int Icmpv4Scan(PIN_ADDR SrcIPv4, PIN_ADDR DstIPv4);
+int Icmpv4Scan(PIN_ADDR SrcIPv4, PIN_ADDR DstIPv4, BYTE Mask);
+int Icmpv6Scan(PIN6_ADDR SrcIPv6, PIN6_ADDR DstIPv6);
+int Icmpv6Scan(PIN6_ADDR SrcIPv6, PIN6_ADDR DstIPv6, BYTE Mask);
