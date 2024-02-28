e640987cc7a72a6fff6a480035a2c4c1b2a11f13
correy <112426112@qq.com>
2024-02-28 10:05:18 +0800

    注释。

--- src/scan/icmp.cpp
+++ src/scan/icmp.cpp
@@ -6,6 +6,13 @@
 
 
 int Icmpv4Scan(PIN_ADDR DstIPv4)
+/*
+
+测试样例代码：
+    IN_ADDR DestinationAddress;
+    DestinationAddress.S_un.S_addr = inet_addr("104.193.88.77");
+    Icmpv4Scan(&DestinationAddress);
+*/
 {
     int ret = 0;
     HANDLE hIcmpFile;
@@ -99,6 +106,13 @@ int Icmpv4Scan(PIN_ADDR DstIPv4, BYTE Mask)
 
 int Icmpv6Scan(PIN6_ADDR DstIPv6)
 /*
+
+测试样例代码：
+    IN6_ADDR sin6_addr;
+    InetPtonA(AF_INET6, "fe80::36ca:81ff:fe23:b43", &sin6_addr); //局域网内的一个链路本地地址。
+    //InetPtonA(AF_INET6, "240e:390:8a0:f940:a09a:948a:3480:4479", &sin6_addr);//局域网内的一个全局IPv6.
+    //InetPtonA(AF_INET6, "2603:1030:20e:3::23c", &sin6_addr);//谨记谷歌的IPv6不翻墙会失败。
+    Icmpv6Scan(&sin6_addr);
 
 局域网的和互联网的都测试成功。
 */
